7acf68304562c7d8cc5ceec174ca7e12b0415256
Mapaa4 <marango322@soyudemedellin.edu.co>
2026-08-04 11:25:55 -0500

    Variables

--- My project (2)/Assets/_Mainj/Scripts/Player movement.cs
+++ My project (2)/Assets/_Mainj/Scripts/Player movement.cs
@@ -4,25 +4,32 @@ public class Playermovement : MonoBehaviour
 {
     // Variables
 
-    [SerializeField] private int _edad = 17;
-    private float _velocidad = 15.5f;
-    private string nombre = "Mapaa";
-    private bool isJumping = false;
+    public float altura = 1.65f;
+    public int edad = 1742;
+    public string nombre = "Pau";
+    public bool puedeVotar = true;
 
-    public float fuerza = 500f;
 
-    [SerializeField] private Rigidbody2D _rigidbody2D;
+    public GameObject gameObject;
+    public Rigidbody2D rigidbody2D;
+    public Collider2D collider2D;
+    public SpriteRenderer spriteRenderer;
+    public Transform tranform;
+    //crear una variable tp trsnfm
 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-       _rigidbody2D.AddForce(Vector2.right * fuerza);
+        Debug.Log("Hola: " + nombre + " tu edad es: " + edad + " tu altura es: " + altura);
+        rigidbody2D.simulated = false;
+        spriteRenderer.color = Color.red; 
+        transform.position = new Vector3(10f, 0f, 0f);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         Debug.Log("Update");
     }
