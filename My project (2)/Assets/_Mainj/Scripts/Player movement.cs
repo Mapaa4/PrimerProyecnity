168f4962ab21324e4d36849c9767ec4bd413d360
Mapaa4 <marango322@soyudemedellin.edu.co>
2026-08-06 11:40:01 -0500

    Saltitoshh

--- My project (2)/Assets/_Mainj/Scripts/Player movement.cs
+++ My project (2)/Assets/_Mainj/Scripts/Player movement.cs
@@ -2,35 +2,28 @@ using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
-    // Variables
 
-    public float altura = 1.65f;
-    public int edad = 1742;
-    public string nombre = "Pau";
-    public bool puedeVotar = true;
-
-
-    public GameObject gameObject;
-    public Rigidbody2D rigidbody2D;
-    public Collider2D collider2D;
-    public SpriteRenderer spriteRenderer;
-    public Transform tranform;
-    //crear una variable tp trsnfm
-
+    [SerializeField] private float _force = 5f;
+    [SerializeField] private float _speed = 1000f;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
-        Debug.Log("Hola: " + nombre + " tu edad es: " + edad + " tu altura es: " + altura);
-        rigidbody2D.simulated = false;
-        spriteRenderer.color = Color.red; 
-        transform.position = new Vector3(10f, 0f, 0f);
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        Debug.Log("Update");
+      if (Input.GetKey(KeyCode.Space))
+     {
+        _rigidbody2D.AddForce(Vector2.up * _force);
+
+     }
+        _rigidbody2D.linearVelocity = Vector2.right * _speed * Time.deltaTime;
+
+
     }
 }
