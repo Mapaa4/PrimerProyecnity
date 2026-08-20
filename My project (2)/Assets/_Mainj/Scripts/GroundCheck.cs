b573ed5cd1ca55d5f24c7926d25f66f327b3ebd0
Mapaa4 <marango322@soyudemedellin.edu.co>
2026-08-20 11:55:52 -0500

    final

--- My project (2)/Assets/_Mainj/Scripts/GroundCheck.cs
+++ My project (2)/Assets/_Mainj/Scripts/GroundCheck.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 public class GroundCheck : MonoBehaviour
 {
     public bool isGround;
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        isGround = true;
-        Debug.Log("Me tocaron");
-    }
+    //private void OnTriggerEnter2D(Collider2D collision)
+  
     private void OnTriggerExit2D(Collider2D collision)
     {
         isGround = false;
         Debug.Log("Me dejaron de tocar");
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        isGround = true;
+        Debug.Log("Me tocaron");
     }
 
 }
